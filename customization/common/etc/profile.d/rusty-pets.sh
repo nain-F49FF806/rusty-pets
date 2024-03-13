17a28cc8d6a3b54677a5d5d576e07a4dbbc57390
nain <126972030+nain-F49FF806@users.noreply.github.com>
2024-03-13 21:56:15 +0100

    Single file init script for more predictable init sequence
    Also add welcome message and tip reg starship

---
+++ customization/common/etc/profile.d/rusty-pets.sh
@@ -0,0 +1,58 @@
+#!/bin/env bash
+
+# Rust binaries location, add to path
+
+[[ ":$PATH:" != *":/opt/cargo/bin:"* ]] && PATH="/opt/cargo/bin:${PATH}"
+
+# Rust aliases
+
+alias ls='eza'
+alias cat='bat --style="header,grid"'
+alias top='btm --battery'
+alias du='dust -p'
+alias time=hyperfine
+alias grep=rg
+alias hexdump=hexyl
+alias diff=delta
+alias find=fd
+
+# Rust tools init
+
+[ "${BASH_VERSION:-}" != "" ] && eval "$(zoxide init bash)"
+[ "${ZSH_VERSION:-}" != "" ] && eval "$(zoxide init zsh)"
+
+[ "${BASH_VERSION:-}" != "" ] && eval "$(starship init bash)"
+[ "${ZSH_VERSION:-}" != "" ] && eval "$(starship init zsh)"
+
+function ya() {
+	local tmp="$(mktemp -t "yazi-cwd.XXXXX")"
+	yazi "$@" --cwd-file="$tmp"
+	if cwd="$(cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
+		cd -- "$cwd"
+	fi
+	rm -f -- "$tmp"
+}
+
+# Rusty pets welcome message (one time)
+
+RP_INTRO_MESSAGE="Welcome !
+You are now in a 🦀rusted🦀 toolbx image, also known as 🔥oxidized🔥 image.
+Here, you will find many Rust-lang based utils for a better shell experience. 
+
+For more info see readme
+https://github.com/nain-F49FF806/rusty-pets?tab=readme-ov-file#what-is-included
+"
+
+if [ ! -f ~/.config/rusty-pets/intro-message-seen ]; then
+	mkdir -p ~/.config/rusty-pets
+    echo "${RP_INTRO_MESSAGE}" | tee ~/.config/rusty-pets/intro-message-seen
+
+	# Tip regarding starship
+	if [ "${BASH_VERSION:-}" != "" ]; then
+        echo "Tip: To activate starship add the following line to your ~/.bashrc"
+		echo 'eval "$(starship init bash)"'
+	fi
+    if [ "${ZSH_VERSION:-}" != "" ]; then
+		echo "Tip: To activate starship add the following line to your ~/.zshrc"
+		echo 'eval "$(starship init zsh)"'
+fi
