5425162b85f8dbcfceba2e0c009c631ef24fbd98
nain <126972030+nain-F49FF806@users.noreply.github.com>
2024-03-13 21:56:15 +0100

    Single file init script for more predictable init sequence
    Also add welcome message and tip reg starship

--- customization/common/etc/profile.d/rusty-aliases.sh
+++
@@ -1,9 +0,0 @@
-alias ls='eza'
-alias cat='bat --style="header,grid"'
-alias top='btm --battery'
-alias du='dust -p'
-alias time=hyperfine
-alias grep=rg
-alias hexdump=hexyl
-alias diff=delta
-alias find=fd
